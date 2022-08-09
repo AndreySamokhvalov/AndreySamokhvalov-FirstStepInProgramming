49fac0b674b78effde4a332d7b977f21056a13ee
Andrey_Samokhvalov <_@example.com123321>
2022-08-09 17:39:49 +0300

    add 24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.Write("Введите число: ");
+
+int inputNumber = int.Parse(Console.ReadLine());
+int t;
+void VariantSimple()
+
+{
+    int sumOfNumbers = 0;
+    for (int i = 1; i <= inputNumber; i++)
+    {
+        sumOfNumbers += i;
+        // sumOfNumbers=sumOfNumbers+i;
+    }
+    Console.WriteLine("сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+void VariantGauss()
+{
+    int sumOfNumbers = 0;
+
+    sumOfNumbers = ((1 + inputNumber) * inputNumber) / 2;
+    Console.WriteLine("сумма чисел от 1 до " + inputNumber + " = " + sumOfNumbers);
+}
+
+t = Environment.TickCount;
+VariantGauss();
+Console.WriteLine("Gauss time : {0} ms", Environment.TickCount - t);
+t = Environment.TickCount;
+VariantSimple();
+Console.WriteLine("Simple time : {0} ms", Environment.TickCount - t);
