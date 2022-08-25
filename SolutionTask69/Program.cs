a3879db98967aad31527089501dc3bbbd5765412
Andrey_Samokhvalov <_@example.com123321>
2022-08-25 19:40:35 +0300

    add 69

---
+++ SolutionTask69/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Clear();
+
+int inputNumberA = ReadNumber("Введите число A: ");
+int inputNumberB = ReadNumber("Введите число B: ");
+
+Console.WriteLine(OriginPow(inputNumberA,inputNumberB));
+Console.WriteLine(PowNumber(inputNumberA,inputNumberB));
+
+
+int OriginPow(int a, int b)
+{
+    if (b == 2)
+    {
+        return a * a;
+    }
+    if (b == 1)
+    {
+        return a;
+    }
+    if (b % 2 == 0)
+    {
+        return OriginPow(a, b / 2) * OriginPow(a, b / 2);
+    }
+    else
+    {
+        return OriginPow(a, b / 2) * OriginPow(a, (b / 2) + 1);
+    }
+}
+
+
+int PowNumber(int a, int b)
+{
+    if (b == 1) return a;
+    return a * PowNumber(a, --b);
+}
+
+//метод считывания данных от пользователя
+int ReadNumber(string text)
+{
+    Console.Write(text);
+    return int.Parse(Console.ReadLine() ?? "");
+}
