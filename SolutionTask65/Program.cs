f304f270647abd50f5164c8eac2d8764a586313f
Andrey_Samokhvalov <_@example.com123321>
2022-08-25 19:39:43 +0300

    add 65

---
+++ SolutionTask65/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+
+if (numberM < numberN)
+{
+    NaturalNamberPrinter(numberM, numberN);
+}
+else
+{
+    NaturalNamberPrinter(numberN, numberM);
+}
+
+//метод считывания данных от пользователя
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+// метод для печати
+void NaturalNamberPrinter(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNamberPrinter(m + 1, n);
+}
