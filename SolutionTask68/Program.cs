263bdc42836d0ed946e9c9da569aec875abb6cd6
Andrey_Samokhvalov <_@example.com123321>
2022-08-25 19:40:17 +0300

    add 68

---
+++ SolutionTask68/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Clear();
+Console.WriteLine("AckermannFunc");
+Console.WriteLine("Welcome!");
+
+int numM = ReadData("задайте число М: ");
+int numN = ReadData("задайте число N: ");
+Console.WriteLine();
+
+Console.Write("А(" + numM + "," + numN + ") = " + AckermannFunc(numM, numN));
+
+// метод получения данных от пользователя
+int ReadData(string data)
+{
+    Console.WriteLine();
+    Console.WriteLine(data);
+    return int.Parse(Console.ReadLine());
+}
+
+// метод вычисления функции Акерманна
+int AckermannFunc(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return AckermannFunc(m - 1, 1);
+    else return AckermannFunc(m - 1, AckermannFunc(m, n - 1));
+}
+
