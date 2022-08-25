43e19f2d18760fd1329d8acdc24d72d69bea2082
Andrey_Samokhvalov <_@example.com123321>
2022-08-25 19:40:01 +0300

    add 67

---
+++ SolutionTask67/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+int num = ReadData("Задайте число: ");
+
+Console.WriteLine(Sum(num));
+
+//метод считывания данных от пользователя
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+// медот для нахождения суммы цифр в числе
+int Sum(int number)
+{
+    if(number>0)
+    {
+        return number%10+Sum(number/10);
+    }
+    else
+    {
+        return 0;
+    }
+}
