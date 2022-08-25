461cdc319a98aac84e43ff73347618ad5a9bb7c8
Andrey_Samokhvalov <_@example.com123321>
2022-08-25 19:38:45 +0300

    add 63

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.Clear();
+
+//метод считывания данных от пользователя
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+// метод печати
+int NuturalNumberPrinter(int num)
+{
+    if(num == 2 )return 1;
+    else
+    {
+        num--;
+        Console.WriteLine(num);
+        Console.Write(NuturalNumberPrinter(num) + " ");
+    }
+    return num;
+}
+
+
+int inputNumber = ReadData();
+NuturalNumberPrinter(inputNumber+2);
